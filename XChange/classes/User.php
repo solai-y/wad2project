d363285f054b0ae63d4f7edb9573a3e23a474322
solai-y <solaiy17112001@gmail.com>
2023-08-20 17:24:34 +0800

    Update User.php
    Added the different datapoints the SQL database has as getter functions

--- XChange/classes/User.php
+++ XChange/classes/User.php
@@ -4,15 +4,31 @@ class User{
     private $username;
     private $password;
 
-    public function __construct($username,$password){
+    public function __construct($campusID, $username,$password, $primaryDegree, $secondaryDegree, $yearOfMatriculation){
+        $this->campusID = $campusID;
         $this->username = $username;
         $this->password = $password;
+        $this->primaryDegree = $primaryDegree;
+        $this->secondaryDegree = $secondaryDegree;
+        $this->yearOfMatriculation = $yearOfMatriculation;
     }
-    public function getUsername(){
+    public function getCampusID(){
+        return $this->campusID;
+    }
+    public function getUser(){
         return $this->username;
     }
     public function getPassword(){
         return $this->password;
+    }
+    public function getPrimaryDegree(){
+        return $this->primaryDegree;
+    }
+    public function getSecondaryDegree(){
+        return $this->secondaryDegree;
+    }
+    public function getYearOfMatriculation(){
+        return $this->yearOfMatriculation;
     }
 }
 
