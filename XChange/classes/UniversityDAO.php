869c6a26a2f8522847ec26b01777e319b5c4975f
solai-y <solaiy17112001@gmail.com>
2023-08-21 20:55:46 +0800

    Pulled data from database onto homepage

--- XChange/classes/UniversityDAO.php
+++ XChange/classes/UniversityDAO.php
@@ -12,11 +12,11 @@ class UniversityDAO {
         $listOfUniversities = array();
 
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        if($row = $stmt->fetch()){
+        while($row = $stmt->fetch()){
             // name, exchange_slots, country, city, picture_url
             $listOfUniversities[] = new University($row['name'], $row['exchange_slots'], $row['country'], $row['city'], $row['picture_url']);
         }
-        
+
         $stmt = null;
         $pdo = null;
         return $listOfUniversities;
