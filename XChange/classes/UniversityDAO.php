6043f4a01e0b011c1eb627a89e48b0683a3751ae
solai-y <solaiy17112001@gmail.com>
2023-08-21 19:08:16 +0800

    Update UniversityDAO.php
    Added get function to pull all the data from the table

--- XChange/classes/UniversityDAO.php
+++ XChange/classes/UniversityDAO.php
@@ -1,4 +1,22 @@
 <?php
+class UniversityDAO {
 
+    public function get() {
+        $conn_manager = new ConnectionManager();
+        $pdo = $conn_manager->getConnection();
+
+        $sql = "select * from universities";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $listOfUniversities = array();
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        if($row = $stmt->fetch()){
+            $listOfUniversities[] = new University()
+        }
+    }
+
+}
 
 ?>
